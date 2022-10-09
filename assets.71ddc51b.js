ab282ca19d17d2e384905ce47b9a46f0cce60e75
jaramirezv <jose.ramirez@openintl.com>
2022-10-09 11:36:56 -0500

    Updates

--- assets.71ddc51b.js
+++ assets.71ddc51b.js
@@ -415,7 +415,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52688" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55333" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
